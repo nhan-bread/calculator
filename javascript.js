83e99aaa6ee8d2ca9e9a8d58c54bfaa0cf19926f
nhan-bread <191059715+nhan-bread@users.noreply.github.com>
2025-10-03 16:22:17 -0700

    Create event listener to update display upon click Only for the digit buttons

--- javascript.js
+++ javascript.js
@@ -7,19 +7,35 @@ let firstNum;
 let operator;
 let secondNum;
 
-function operate(first, second) {
-    switch(operator) {
-        case "+":
-            add(first, second);
-            break;
-        case "-":
-            subtract(first, second);
-            break;
-        case "*":
-            multiply(first, second);
-            break;
-        case "÷":
-            divide(first, second);
-            break;
+const btns = Array.from(document.querySelectorAll("button"));
+const display = document.querySelector(".display");
+
+btns.forEach((btn) => btn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("digit") && (!operator)) {
+        display.textContent += e.target.textContent;
     }
-}
+    if (e.target.classList.contains("digit") && (operator)) {
+        if (!secondNum) {
+            display.textContent = e.target.textContent;
+        } else {
+            display.textContent += e.target.textContent;
+        }
+    }
+
+    function operate(first, second) {
+        switch(operator) {
+            case "+":
+                add(first, second);
+                break;
+            case "-":
+                subtract(first, second);
+                break;
+            case "*":
+                multiply(first, second);
+                break;
+            case "÷":
+                divide(first, second);
+                break;
+        }
+    }
+}))

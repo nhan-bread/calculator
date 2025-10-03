221608fa9594267fc97659c61d0a33cd97c8f002
nhan-bread <191059715+nhan-bread@users.noreply.github.com>
2025-10-03 16:02:29 -0700

    Create operate() to evaluate numbers

--- javascript.js
+++ javascript.js
@@ -6,3 +6,20 @@ let divide = (first, second) => first / second;
 let firstNum;
 let operator;
 let secondNum;
+
+function operate(first, second) {
+    switch(operator) {
+        case "+":
+            add(first, second);
+            break;
+        case "-":
+            subtract(first, second);
+            break;
+        case "*":
+            multiply(first, second);
+            break;
+        case "÷":
+            divide(first, second);
+            break;
+    }
+}

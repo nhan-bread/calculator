b96173f12f2b38ceeb972d6ebe504d90f2556b4f
nhan-bread <191059715+nhan-bread@users.noreply.github.com>
2025-10-03 15:43:29 -0700

    Create variables to store button inputs

--- javascript.js
+++ javascript.js
@@ -2,3 +2,7 @@ let add = (first, second) => first + second;
 let subtract = (first, second) => first - second;
 let multiply = (first, second) => first * second;
 let divide = (first, second) => first / second;
+
+let firstNum;
+let operator;
+let secondNum;

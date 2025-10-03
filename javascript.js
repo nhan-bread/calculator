5875e5164cf0433fcf22c64a7a7694031d05fff3
nhan-bread <191059715+nhan-bread@users.noreply.github.com>
2025-10-03 15:41:43 -0700

    Create functions for basic math operators

---
+++ javascript.js
@@ -0,0 +1,4 @@
+let add = (first, second) => first + second;
+let subtract = (first, second) => first - second;
+let multiply = (first, second) => first * second;
+let divide = (first, second) => first / second;
